0cfedf75608a1a1299d7ed49b6ad16cb6cfddce0
irsyadrolandi <106064708+irsyadrolandi@users.noreply.github.com>
2022-05-24 03:50:02 +0700

    Update TugasakhirMuhammadIrsyadRolandi.py

--- TugasakhirMuhammadIrsyadRolandi.py
+++ TugasakhirMuhammadIrsyadRolandi.py
@@ -1,16 +1,19 @@
 #List Modul yang digunakan dalam Tugas ini : 
 # Modul 1 : Variabel
 # Modul 2 : if-else
+# Modul 3 : For
 # Modul 4 : Function
 # Modul 7 : Stack
 # Modul 8 : GUI
 
 
+import random
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import Progressbar
 from tkinter.messagebox import *
 from turtle import window_width
+import time
 
 Nilai = []
 window = Tk()
@@ -41,8 +44,8 @@ n_uts.grid(column=1,row=3)
 uas.grid(column=0,row=4)
 n_uas.grid(column=1,row=4)
 pemberitahuan.grid(column=0, row=5)
-#membuat tampilan tombol
-def mulai(): #fungsi tombol
+#Tampilan tombol
+def mulai(): #function tombol
     a = int(n_hadir.get())
     b = int(n_tugas.get())
     c = int(n_uts.get())
@@ -64,7 +67,7 @@ def mulai(): #fungsi tombol
         n_hasil.insert(0,str(has))
         messagebox.showinfo("Nilai Konversi","Nilai Anda adalah E (TINGKATKAN YA)")
 
-#membuat tampilan hasil
+#Tampilan hasil
 hasil = Label(root,text="Nilai Anda Adalah : ")
 n_hasil = Entry(root,width=10,borderwidth=5)
 hasil.grid(column=0, row = 6)
@@ -73,7 +76,8 @@ n_hasil.grid(column=1,row=6)
 olah = Button(root,text="klik",bg="light blue",command=mulai)
 olah.grid(columnspan=2,rowspan=2)
 
-#Membuat Stack
+#Stack
+window.title("Data Nilai Siswa")
 def stackPush():
     data = userInput.get()
     Nilai.append(data)
@@ -82,7 +86,7 @@ def stackPop():
     Nilai.pop()
 
 
-labelnama = Label(window,text = "Masukkan Nilai Rata Rata yang sudah dihitung : \n").pack(side=TOP)
+labelNilai = Label(window,text = "Masukkan Nilai Rata Rata yang sudah dihitung : \n").pack(side=TOP)
 userInput = StringVar()
 entry = Entry(window,textvariable=userInput).pack(side=TOP)
 
@@ -90,20 +94,53 @@ def tampilData():
     global showNama
     showNama.configure(state="normal")
     showNama.delete(1.0,"end")
-    showNama.insert("end", "Nilai Rata Rata  : \n")
+    showNama.insert("end", "Nama Dan Nilai  : \n")
     showNama.insert("end", '\n'.join(Nilai))
     showNama.configure(state="disabled")
 
 #buttons
 buttonSubmit = Button(window, text="Submit",bg="light green", command=lambda: [stackPush(), tampilData()]).pack(pady=5)
-buttonPop = Button(window, text= "Pop Data",bg="light blue", command=lambda: [stackPop(), tampilData()]).pack(pady=5)
+buttonPop = Button(window, text= "Delete",bg="light blue", command=lambda: [stackPop(), tampilData()]).pack(pady=5)
 
 #tampil Nilai Rata Rata
-showNama = Text(window, width=30, height= 10)
-showNama.insert("end", "Nilai Rata Rata  : ")
+showNama = Text(window, width=25, height= 8)
+showNama.insert("end", "Nama Dan Nilai  : ")
 showNama.insert("end", '\n'.join(Nilai))
 showNama.configure(state="disabled")
 showNama.pack(pady=5)
+
+#loading bar dan random function Bonus
+def randomFunction():
+    global showWinner
+    pb['value'] = 0
+    for i in range(100):
+        window.update_idletasks()
+        pb['value'] += 1
+        time.sleep(1/100)
+    showWinner.configure(state="normal")
+    if Nilai:
+        winner = random.choice(Nilai)
+        Nilai.remove(winner)
+        showWinner.insert("end","Selamat kepada ")
+        showWinner.insert("end", winner)
+        showWinner.insert("end", '\n')
+    else:
+        messagebox.showwarning("Perhatian!", "Tidak ada yang dapat bonus")
+    showWinner.configure(state="disabled")
+
+buttonGenerate = Button(window, text="Cari Pemenang",bg="light blue", command=lambda: [randomFunction(), tampilData()]).pack()
+showWinner = Text(window, width=20, height= 3)
+showWinner.insert("end", "YANG MENDAPAT BONUS ADALAH : \n")
+showWinner.configure(state="disabled")
+showWinner.pack(pady=5)
+
+pb = Progressbar(
+    window,
+    orient = HORIZONTAL,
+    length = 250,
+    mode = 'determinate'
+    )
+pb.pack(side= BOTTOM, pady=35)
 
 window.mainloop()
 root.mainloop()
